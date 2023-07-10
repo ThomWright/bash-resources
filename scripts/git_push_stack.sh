199cd4e8967a98da61a6e864c5dd6b7fd9959d82
Thom Wright <dev@thomwright.co.uk>
2023-07-10 14:58:27 +0100

    Add force option to git push stack

--- scripts/git_push_stack.sh
+++ scripts/git_push_stack.sh
@@ -9,15 +9,30 @@ usage() {
   cat <<EOF
 Push a stack of branches.
 
-Examples:
+USAGE:
 
-Push all branches for \`my-feature\`:
-  git_push_stack my-feature
+  git_push_stack [OPTIONS] BRANCH_PATTERN
 
-Dependencies:
+ARGS:
 
-- grep
-- git
+  <BRANCH_PATTERN>
+    A pattern matching a set of branches, using grep syntax.
+
+OPTIONS:
+
+  -f, --force
+    Forces the git push.
+
+EXAMPLES:
+
+  Push all branches for \`my-feature\`:
+
+    git_push_stack my-feature
+
+DEPENDENCIES:
+
+  - grep
+  - git
 EOF
 }
 
@@ -25,14 +40,49 @@ log() {
   echo -e "${1:-}" >&2
 }
 
-if [ -z "${1+x}" ]; then
-  log "Error: Missing branch pattern argument"
-  log
-  usage
-  exit 1
-fi
-pattern=$1
+parse_params() {
+  # Set defaults for optional arguments
+  force=${force:-false}
+
+  while [[ $# -gt 0 ]]; do
+    local key="$1"
+
+    case $key in
+    -f | --force)
+      force=true
+      ;;
+    -h | --help)
+      usage
+      exit
+      ;;
+    --* | -*)
+      log "❌ Error: Unsupported flag: $1"
+      exit 1
+      ;;
+    *) # preserve positional params
+      if [ -z "${branch_pattern+x}" ]; then
+        branch_pattern="$1"
+      else
+        log "🙈 More than one branch pattern set"
+        exit 1
+      fi
+      ;;
+    esac
+    shift
+  done
+
+  # Check required arguments
+  local req_args="branch_pattern" # space-separated list
+  for arg in $req_args; do
+    if [ -z "${!arg+x}" ]; then
+      log "🙈 Missing required parameter: $arg"
+      exit 1
+    fi
+  done
+}
+
+parse_params "$@"
 
 git for-each-ref --format='%(refname:short)' refs/heads/ \
-  | grep "$pattern" \
+  | grep "$branch_pattern" \
   | xargs git push --atomic --set-upstream origin
